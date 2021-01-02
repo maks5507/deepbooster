31c39163395628c7a16f8102cc41dd106a7b56e1
Maksim Eremeev <maks5507@yandex.ru>
2021-01-03 02:48:12 +0300

    setup fix 2

---
+++ build/lib/deepbooster/launcher/__init__.py
@@ -0,0 +1,6 @@
+#
+# Created by maks5507 (me@maksimeremeev.com)
+#
+
+
+from .launcher import Launcher
---
+++ build/lib/deepbooster/trainer/__init__.py
@@ -0,0 +1,5 @@
+#
+# Created by maks5507 (me@maksimeremeev.com)
+#
+
+from .trainer import Trainer
