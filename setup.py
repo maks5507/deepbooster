a4e50a750d0ad26f473398188982c20034d6e87e
Maksim Eremeev <maks5507@yandex.ru>
2021-01-03 02:40:49 +0300

    slight refactor  + setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+#
+# Created by maks5507 (me@maksimeremeev.com)
+#
+
+from setuptools import setup, find_packages
+import setuptools.command.build_py as build_py
+
+
+setup_kwargs = dict(
+    name='deepbooster',
+    version='0.0.1',
+    packages=['deepbooster'],
+    install_requires=[
+        'torch',
+        'typing',
+        'pickle',
+        'json',
+        'tqdm',
+        'rmq_interface'
+    ],
+    setup_requires=[
+    ],
+
+    cmdclass={'build_py': build_py.build_py},
+)
+
+setup(**setup_kwargs)
